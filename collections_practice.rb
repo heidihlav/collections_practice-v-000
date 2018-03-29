fdccd3624391a85266db6b6a36c92c6e9900150f
Heidi Hlavinka <hchlav@gmail.com>
2018-03-29 14:10:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|
+  [1,2].each_with_index.collect do |element, index|
     "#{element}" + "s"
   end
 end
