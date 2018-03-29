50bf3f7cb2a4ce159aab0455aaa5920dea52a2f4
Heidi Hlavinka <hchlav@gmail.com>
2018-03-29 14:47:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,9 +15,9 @@ def sort_array_char_count(array)
   array.sort_by { |word| word.length}
 end
 
-def swap_elements(array) #NEED TO REFACTOR
+def swap_elements(array)
   array = ["blake", "ashley", "scott"]
-  [1,2] = [2,1]
+  array[1,2] = array[2,1]
   array
 end
 
